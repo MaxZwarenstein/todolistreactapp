8a86e41492834c517fe7e183dacb7196c2155de6
Max Zwarenstein <maxzwarenstein@gmail.com>
2019-04-24 21:49:09 -0400

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+        <h1>About</h1>
+        <p>This is my Todo List app v1.0.0. It is my first React project.</p>
+    </React.Fragment>
+  )
+}
+
+export default About;
